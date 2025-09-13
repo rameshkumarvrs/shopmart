134e94989189451f7c181ae0fe7965b0feb7d8cc
ramesh <rameshkumar.v@celestialsys.com>
2025-09-13 12:07:28 +0530

    added the alayout page dynamically

--- shop/urls.py
+++ shop/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("home", views.home, name="home")
+    path("home", views.home, name="home"),
+    path("register", views.register, name="register")
 
 ]
--- shop/views.py
+++ shop/views.py
@@ -4,3 +4,6 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, "shop/index.html")
+
+def register(request):
+    return render(request, "shop/register.html")
