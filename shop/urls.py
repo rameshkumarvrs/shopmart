0a19739aee09b31e704db9663299d27068c8c2f1
ramesh <rameshkumar.v@celestialsys.com>
2025-09-14 14:57:07 +0530

    added carosel baners using bootstrap5

--- shop/urls.py
+++ shop/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("home", views.home, name="home"),
+    path("", views.home, name="home"),
     path("register", views.register, name="register")
 
 ]
--- shopmart/settings.py
+++ shopmart/settings.py
@@ -129,6 +129,11 @@ MEDIA_URL='/images/'
 MEDIA_ROOT=BASE_DIR/'static'
 
 
+STATICFILES_DIRS = [
+    BASE_DIR/'static'
+]
+
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
